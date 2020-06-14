99d939dad960dfb01ee00e954161059ded1c00ff
jiangchuan <jiangchuan.huang@gmail.com>
2020-06-13 21:53:29 -0500

    delete led

--- src/livox_razor_node.cpp
+++ src/livox_razor_node.cpp
@@ -14,14 +14,12 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Transform.h>
 
-// #include <wiringPi.h>
-
-#include <errno.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
+// #include <errno.h>
+// #include <fcntl.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <sys/types.h>
+// #include <unistd.h>
 
 #define ROS_RATE 20
 #define SAVE_SIZE 10000
@@ -220,53 +218,42 @@ int main(int argc, char **argv)
 {
     if (use_pi)
     {
-        // wiringPiSetup();
-        // pinMode(0, OUTPUT);
+        // // Export the desired pin by writing to /sys/class/gpio/export
+        // fd = open("/sys/class/gpio/export", O_WRONLY);
+        // if (fd == -1)
+        // {
+        //     perror("Unable to open /sys/class/gpio/export");
+        //     exit(1);
+        // }
 
-        ////////////////////
-        // Export the desired pin by writing to /sys/class/gpio/export
+        // if (write(fd, "24", 2) != 2)
+        // {
+        //     perror("Error writing to /sys/class/gpio/export");
+        //     exit(1);
+        // }
+        // close(fd);
 
-        fd = open("/sys/class/gpio/export", O_WRONLY);
-        if (fd == -1)
-        {
-            perror("Unable to open /sys/class/gpio/export");
-            exit(1);
-        }
+        // // Set the pin to be an output by writing "out" to /sys/class/gpio/gpio24/direction
+        // fd = open("/sys/class/gpio/gpio24/direction", O_WRONLY);
+        // if (fd == -1)
+        // {
+        //     perror("Unable to open /sys/class/gpio/gpio24/direction");
+        //     exit(1);
+        // }
+        // if (write(fd, "out", 3) != 3)
+        // {
+        //     perror("Error writing to /sys/class/gpio/gpio24/direction");
+        //     exit(1);
+        // }
+        // close(fd);
+        // fd = open("/sys/class/gpio/gpio24/value", O_WRONLY);
+        // if (fd == -1)
+        // {
+        //     perror("Unable to open /sys/class/gpio/gpio24/value");
+        //     exit(1);
+        // }
 
-        if (write(fd, "24", 2) != 2)
-        {
-            perror("Error writing to /sys/class/gpio/export");
-            exit(1);
-        }
-
-        close(fd);
-
-        // Set the pin to be an output by writing "out" to /sys/class/gpio/gpio24/direction
-
-        fd = open("/sys/class/gpio/gpio24/direction", O_WRONLY);
-        if (fd == -1)
-        {
-            perror("Unable to open /sys/class/gpio/gpio24/direction");
-            exit(1);
-        }
-
-        if (write(fd, "out", 3) != 3)
-        {
-            perror("Error writing to /sys/class/gpio/gpio24/direction");
-            exit(1);
-        }
-
-        close(fd);
-
-        fd = open("/sys/class/gpio/gpio24/value", O_WRONLY);
-        if (fd == -1)
-        {
-            perror("Unable to open /sys/class/gpio/gpio24/value");
-            exit(1);
-        }
-        ////////////////////
-
-        rootdir = "/home/pi/livox_data/";
+        rootdir = "/home/ubuntu/livox_data/";
     }
     else
     {
@@ -307,33 +294,31 @@ int main(int argc, char **argv)
             saveRawData(gps_msg, livox_msg);
         }
 
-        if (use_pi && spin_count % LED_JUMP == 0)
-        {
-            set_led();
-        }
+        // if (use_pi && spin_count % LED_JUMP == 0)
+        // {
+        //     set_led();
+        // }
         spin_count++;
 
         ros::spinOnce();
         rate.sleep();
     }
 
-    close(fd);
+    // close(fd);
 
-    // Unexport the pin by writing to /sys/class/gpio/unexport
-
-    fd = open("/sys/class/gpio/unexport", O_WRONLY);
-    if (fd == -1)
-    {
-        perror("Unable to open /sys/class/gpio/unexport");
-        exit(1);
-    }
-
-    if (write(fd, "24", 2) != 2)
-    {
-        perror("Error writing to /sys/class/gpio/unexport");
-        exit(1);
-    }
-
-    close(fd);
+    // // Unexport the pin by writing to /sys/class/gpio/unexport
+    // fd = open("/sys/class/gpio/unexport", O_WRONLY);
+    // if (fd == -1)
+    // {
+    //     perror("Unable to open /sys/class/gpio/unexport");
+    //     exit(1);
+    // }
+    // if (write(fd, "24", 2) != 2)
+    // {
+    //     perror("Error writing to /sys/class/gpio/unexport");
+    //     exit(1);
+    // }
+    // close(fd);
+    
     return 0;
 }
