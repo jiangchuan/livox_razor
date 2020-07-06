be98b25df5effc3a2155bdbf71ecb06d2ddc4c03
jiangchuan <jiangchuan.huang@gmail.com>
2020-07-06 09:59:37 -0500

    Good CSV2

--- src/livox_razor_node.cpp
+++ src/livox_razor_node.cpp
@@ -59,23 +59,11 @@ std::string get_time_str()
     return std::to_string(year) + "-" + digit2str(month) + "-" + digit2str(day) + "_" + digit2str(hour) + "-" + digit2str(minute) + "-" + digit2str(second);
 }
 
-void set_led()
-{
-    if (second % 2 == 0)
-    {
-        gpioWrite(24, 1); /* on */
-    }
-    else
-    {
-        gpioWrite(24, 0); /* off */
-    }
-}
-
 void saveRawData(sensor_msgs::PointCloud2::ConstPtr livox_msg)
 {
+    time_t now = get_time();
     if (imu_msg && gps_msg && livox_msg)
     {
-        time_t now = get_time();
         std::string time_str = get_time_str();
 
         boost::thread::id this_id = boost::this_thread::get_id();
@@ -150,7 +138,14 @@ void saveRawData(sensor_msgs::PointCloud2::ConstPtr livox_msg)
             file << stream.rdbuf();
             file.close();
         }
-        set_led();
+        if (second % 2 == 1)
+        {
+            gpioWrite(24, 1); /* on */
+        }
+    }
+    if (second % 2 == 0)
+    {
+        gpioWrite(24, 0); /* off */
     }
 }
 
@@ -162,10 +157,6 @@ void imu_callback(const sensor_msgs::Imu::ConstPtr &msg)
 void gps_callback(const sensor_msgs::NavSatFix::ConstPtr &msg)
 {
     gps_msg = msg;
-    if (second % 2 == 1)
-    {
-        gpioWrite(24, 0); /* off */
-    }
 }
 
 void livox_callback(const sensor_msgs::PointCloud2::ConstPtr &msg)
