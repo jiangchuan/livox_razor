877452b8ce0b0c21e997f8e1224e728901f51e8c
jiangchuan <jiangchuan.huang@gmail.com>
2020-07-06 10:45:50 -0500

    Good CSV3

--- src/livox_razor_csv.cpp
+++ src/livox_razor_csv.cpp
@@ -56,19 +56,7 @@ std::string digit2str(int num)
 
 std::string get_time_str()
 {
-    return std::to_string(year) + "-" + digit2str(month) + "-" + digit2str(day) + "_" + digit2str(hour) + "-" + digit2str(minute) + "-" + digit2str(second);
-}
-
-void set_led()
-{
-    if (second % 2 == 0)
-    {
-        gpioWrite(24, 1); /* on */
-    }
-    else
-    {
-        gpioWrite(24, 0); /* off */
-    }
+    return std::to_string(year) + "-" + digit2str(month) + "-" + digit2str(day) + "_" + digit2str(hour) + "-" + digit2str(minute);
 }
 
 void saveRawData(sensor_msgs::PointCloud2::ConstPtr livox_msg)
@@ -100,7 +88,7 @@ void saveRawData(sensor_msgs::PointCloud2::ConstPtr livox_msg)
         }
 
         std::stringstream ss;
-        ss << timedir << time_str << "_" << this_id << "_" << num_writes_map[this_id] << ".csv";
+        ss << timedir << time_str << "-" << digit2str(second) << "_" << this_id << "_" << num_writes_map[this_id] << ".csv";
         gps_filename = ss.str();
 
         sensor_msgs::PointCloud pt_cloud;
@@ -150,7 +138,10 @@ void saveRawData(sensor_msgs::PointCloud2::ConstPtr livox_msg)
             file << stream.rdbuf();
             file.close();
         }
-        set_led();
+        if (second % 2 == 1)
+        {
+            gpioWrite(24, 1); /* on */
+        }
     }
 }
 
@@ -162,7 +153,11 @@ void imu_callback(const sensor_msgs::Imu::ConstPtr &msg)
 void gps_callback(const sensor_msgs::NavSatFix::ConstPtr &msg)
 {
     gps_msg = msg;
-    if (second % 2 == 1)
+
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    second = ltm->tm_sec;
+    if (second % 2 == 0)
     {
         gpioWrite(24, 0); /* off */
     }
