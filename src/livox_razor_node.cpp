638aabed2c7097d2318b2e6adec242299889605e
jiangchuan <jiangchuan.huang@gmail.com>
2020-08-22 21:52:16 -0500

    Save individual1

--- src/livox_razor_node.cpp
+++ src/livox_razor_node.cpp
@@ -19,16 +19,15 @@
 #define ROS_RATE 20
 #define SAVE_SIZE 500
 
-sensor_msgs::Imu::ConstPtr imu_msg;
-sensor_msgs::NavSatFix::ConstPtr gps_msg;
-
 int year = 0, month = 0, day = 0, hour = 0, minute = 0, second = 0;
 std::string lidar_dir;
-std::string timedir;
-std::string lidar_filename;
-
-std::map<boost::thread::id, int> num_writes_map;
-std::map<boost::thread::id, time_t> last_time_map;
+std::string imu_dir;
+std::string lidar_timedir;
+std::string imu_timedir;
+std::map<boost::thread::id, int> lidar_num_writes_map;
+std::map<boost::thread::id, time_t> lidar_last_time_map;
+std::map<boost::thread::id, int> imu_num_writes_map;
+std::map<boost::thread::id, time_t> imu_last_time_map;
 
 inline double to_time(sensor_msgs::PointCloud2ConstPtr msg) {
     return (msg->header.stamp.sec) * 1.0 + (msg->header.stamp.nsec / 1000000000.0);
@@ -62,28 +61,28 @@ std::string get_time_str() {
     return std::to_string(year) + "-" + digit2str(month) + "-" + digit2str(day) + "_" + digit2str(hour) + "-" + digit2str(minute);
 }
 
-void saveRawData(sensor_msgs::PointCloud2ConstPtr lidar_msg) {
+void save_lidar_data(sensor_msgs::PointCloud2ConstPtr lidar_msg) {
     if (lidar_msg) {
         time_t now = get_time();
         std::string time_str = get_time_str();
         boost::thread::id this_id = boost::this_thread::get_id();
-        if (last_time_map.find(this_id) == last_time_map.end()) {
-            last_time_map[this_id] = 0;
+        if (lidar_last_time_map.find(this_id) == lidar_last_time_map.end()) {
+            lidar_last_time_map[this_id] = 0;
         }
-        if (difftime(now, last_time_map[this_id]) > 60) {
-            timedir = lidar_dir + time_str + "/";
-            int status = mkdir(timedir.c_str(), 0777);
-            last_time_map[this_id] = now;
+        if (difftime(now, lidar_last_time_map[this_id]) > 60) {
+            lidar_timedir = lidar_dir + time_str + "/";
+            int status = mkdir(lidar_timedir.c_str(), 0777);
+            lidar_last_time_map[this_id] = now;
         }
-        if (num_writes_map.find(this_id) == num_writes_map.end()) {
-            num_writes_map[this_id] = 0;
+        if (lidar_num_writes_map.find(this_id) == lidar_num_writes_map.end()) {
+            lidar_num_writes_map[this_id] = 0;
         } else {
-            num_writes_map[this_id]++;
+            lidar_num_writes_map[this_id]++;
         }
 
         std::stringstream ss;
-        ss << timedir << time_str << "-" << digit2str(second) << "_" << this_id << "_" << num_writes_map[this_id] << ".csv";
-        lidar_filename = ss.str();
+        ss << lidar_timedir << time_str << "-" << digit2str(second) << "_" << this_id << "_" << lidar_num_writes_map[this_id] << ".csv";
+        std::string lidar_filename = ss.str();
 
         sensor_msgs::PointCloud pt_cloud;
         sensor_msgs::convertPointCloud2ToPointCloud(*lidar_msg, pt_cloud);
@@ -137,28 +136,60 @@ void saveRawData(sensor_msgs::PointCloud2ConstPtr lidar_msg) {
     }
 }
 
-void imu_callback(const sensor_msgs::Imu::ConstPtr &msg) {
-    imu_msg = msg;
+void imu_callback(const sensor_msgs::Imu::ConstPtr &imu_msg) {
+    time_t now = get_time();
+    std::string time_str = get_time_str();
+    boost::thread::id this_id = boost::this_thread::get_id();
+    if (imu_last_time_map.find(this_id) == imu_last_time_map.end()) {
+        imu_last_time_map[this_id] = 0;
+    }
+    if (difftime(now, imu_last_time_map[this_id]) > 60) {
+        imu_timedir = imu_dir + time_str + "/";
+        int status = mkdir(imu_timedir.c_str(), 0777);
+        imu_last_time_map[this_id] = now;
+    }
+    if (imu_num_writes_map.find(this_id) == imu_num_writes_map.end()) {
+        imu_num_writes_map[this_id] = 0;
+    } else {
+        imu_num_writes_map[this_id]++;
+    }
+
+    std::stringstream ss;
+    ss << imu_timedir << time_str << "-" << digit2str(second) << "_" << this_id << "_" << imu_num_writes_map[this_id] << ".csv";
+    std::string imu_filename = ss.str();
+
+    std::stringstream stream;
+    stream << std::setprecision(4) << imu_msg->orientation.x << ",";
+    stream << std::setprecision(4) << imu_msg->orientation.y << ",";
+    stream << std::setprecision(4) << imu_msg->orientation.z << ",";
+    stream << std::setprecision(4) << imu_msg->orientation.w << ",";
+
+    std::fstream file;
+    file.open(imu_filename, std::ios::out | std::ios::app);
+    file << stream.rdbuf();
+    file.close();
 }
 
 void gps_callback(const sensor_msgs::NavSatFix::ConstPtr &msg) {
-    gps_msg = msg;
+    // gps_msg = msg;
 }
 
 void livox_callback(const sensor_msgs::PointCloud2ConstPtr &msg) {
-    saveRawData(msg);
+    save_lidar_data(msg);
 }
 
 int main(int argc, char **argv) {
     std::string root_dir = "/home/ubuntu/livox_data/";
     lidar_dir = root_dir + "lidar/";
+    imu_dir = root_dir + "imu/";
     int status = mkdir(root_dir.c_str(), 0777);
     status = mkdir(lidar_dir.c_str(), 0777);
+    status = mkdir(imu_dir.c_str(), 0777);
 
     ros::init(argc, argv, "livox_razor");
     ros::NodeHandle nh;
     // ros::Subscriber imu_sub = nh.subscribe<sensor_msgs::Imu>("imu", 1, imu_callback);          // Razor IMU
-    ros::Subscriber imu_sub = nh.subscribe<sensor_msgs::Imu>("imu/data", 1, imu_callback);     // Adi IMU
+    ros::Subscriber imu_sub = nh.subscribe<sensor_msgs::Imu>("imu/data", 1, imu_callback);     // Adis IMU
     ros::Subscriber gps_sub = nh.subscribe<sensor_msgs::NavSatFix>("qxgps", 1, gps_callback);  // QX GPS
     ros::Subscriber livox_sub = nh.subscribe<sensor_msgs::PointCloud2>("livox/lidar", 1, livox_callback);
     ros::Rate rate((double)ROS_RATE);  // The setpoint publishing rate MUST be faster than 2Hz
